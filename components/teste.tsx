3238c29ff2e22d9cfc69df7d8bdf7e8825bc9136
Felipe <felipi@live.com>
2024-08-01 17:25:21 -0300

    refactor: Update Card background color to red in teste.tsx

--- components/teste.tsx
+++ components/teste.tsx
@@ -10,7 +10,7 @@ function Teste({ children }) {
     return null;
   }
   return (
-    <Card className="h-32 p-6 bg-violet-100 ring-0 w-full">
+    <Card className="h-32 p-6 bg-red-200 ring-0 w-full">
       <div className="flex justify-between items-center">{children}</div>
     </Card>
   );
