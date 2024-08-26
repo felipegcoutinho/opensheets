3facce689fa31b9a03f0b6a5a1db95cf4ff628ac
Felipe <felipi@live.com>
2024-08-26 00:34:55 -0300

    refactor: Update Card component layout and currency formatting in multiple components

---
+++ app/investimentos/chart.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
+import React from "react";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+const chartConfig = {
+  views: {
+    label: "Valor",
+  },
+  desktop: {
+    color: "hsl(var(--chart-3))",
+  },
+} satisfies ChartConfig;
+
+export default function InvestComponent({ data }) {
+  const valorAtual = React.useMemo(() => {
+    return data.length > 0 ? data[data.length - 1].valor : 0;
+  }, [data]);
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-4">
+          <CardTitle>Investimentos</CardTitle>
+          <CardDescription>Monitore seus investimentos</CardDescription>
+        </div>
+        <div className="flex">
+          <div className="flex flex-1 flex-col justify-center gap-1 border-t py-4 text-left sm:border-t-0 sm:px-6 sm:py-6">
+            <span className="text-xs text-muted-foreground">Valor Atual</span>
+            <span className="text-lg font-bold leading-none sm:text-2xl">
+              {Number(valorAtual).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+            </span>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="px-2 sm:p-6">
+        <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-full">
+          <BarChart
+            accessibilityLayer
+            data={data}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="data"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value);
+                const utcDate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+                return utcDate.toLocaleDateString("pt-BR", {
+                  month: "short",
+                  day: "numeric",
+                });
+              }}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  className="w-[150px]"
+                  nameKey="views"
+                  labelFormatter={(value) => {
+                    const date = new Date(value);
+                    const utcDate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+                    return utcDate.toLocaleDateString("pt-BR", {
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+              }
+            />
+            <Bar dataKey="valor" fill={`var(--color-desktop)`} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ app/investimentos/page.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { UseDates } from "@/hooks/UseDates";
+import { getInvest } from "../actions/invest";
+import InvestComponent from "./chart";
+import CreateInvestimento from "./modal/create-invest";
+import DeleteInvest from "./modal/delete-invest";
+import UpdateInvest from "./modal/update-invest";
+
+async function page() {
+  const { DateFormat } = UseDates();
+  const investData = await getInvest();
+
+  const calculateDifference = (currentValue, previousValue) => {
+    return currentValue - previousValue;
+  };
+
+  return (
+    <div className="mt-4 w-full">
+      <CreateInvestimento>
+        <Button variant="default">Lançar Investimento</Button>
+      </CreateInvestimento>
+
+      <div className="w-full mt-4">
+        <InvestComponent data={investData} />
+      </div>
+
+      <Table className="w-full mt-4">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Data</TableHead>
+            <TableHead>Valor</TableHead>
+            <TableHead>Diferença</TableHead>
+            <TableHead>Ações</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {investData.map((item, index) => {
+            const previousItem = investData[index - 1];
+            const difference = previousItem ? calculateDifference(item.valor, previousItem.valor) : 0;
+
+            // Classe CSS condicional com base na diferença
+            const differenceClass = difference > 0 ? "text-green-500" : difference < 0 ? "text-red-500" : "text-black dark:text-white";
+
+            return (
+              <TableRow key={item.id} className="whitespace-nowrap">
+                <TableCell className="font-bold">{item.id}</TableCell>
+                <TableCell>{DateFormat(item.data)}</TableCell>
+                <TableCell>{Number(item.valor).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</TableCell>
+                <TableCell className={differenceClass}>
+                  {difference ? difference.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }) : "-"}
+                </TableCell>
+                <TableCell>
+                  <UpdateInvest itemId={item.id} itemData={item.data} itemValor={item.valor} />
+                  <span className="pl-2">
+                    <DeleteInvest itemId={item.id} />
+                  </span>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+export default page;
