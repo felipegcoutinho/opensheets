050f0fbae2c585191e69ffdd02bafa52a3e72b9c
felipegcoutinho <felipi@live.com>
2025-03-12 11:32:16 -0300

    Add Umami analytics script to RootLayout for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { PrivacyProviderApp } from "@/hooks/privacy-context";
 import { ThemeProvider } from "@/hooks/use-dark-mode";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 import { uiSans } from "./fonts/font";
 import "./globals.css";
 
@@ -32,6 +33,11 @@ export default function RootLayout({ children }) {
           <SpeedInsights />
           <Analytics />
           <Toaster position="top-right" richColors duration={2500} />
+          <Script
+            defer
+            src="https://umami.felipecoutinho.com/script.js"
+            data-website-id="0c7a2975-d404-4893-b603-6f598dfc8751"
+          />
         </ThemeProvider>
       </body>
     </html>
