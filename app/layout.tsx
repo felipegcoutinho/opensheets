7c305c5a80f0047960d0f2e3a50b4bbf6a5e4b60
Felipe <felipi@live.com>
2025-05-04 11:12:42 -0300

    feat: add ReactScan component for enabling react-scan functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import Script from "next/script";
 import { openRunde } from "./fonts/font";
 import "./globals.css";
+import { ReactScan } from "@/components/scan";
 
 export const metadata = {
   title: "opensheets",
@@ -24,6 +25,10 @@ export default function RootLayout({
       className={`${openRunde.className} `}
       suppressHydrationWarning
     >
+      <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+      </head>
+      <ReactScan />
       <body>
         <ThemeProvider
           attribute="class"
