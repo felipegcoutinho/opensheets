39bd192e4ed50732c58550639ebc28c0ecb837ec
Felipe Coutinho <felipi@live.com>
2025-07-01 18:30:25 -0300

    style: Ajustar estilos e classes em vários componentes
    - Atualiza a classe do link no AccountWidget para incluir 'font-medium'.
    - Modifica a altura da barra de progresso no PaymentSection.
    - Altera a cor de fundo no MoneyValues para 'text-emerald-900'.
    - Remove a borda no Card para um visual mais limpo.
    - Atualiza a cor de fundo no globals.css para um valor mais apropriado.

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded border py-6 dark:border-none",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded border-none py-6 dark:border-none",
         className,
       )}
       {...props}
