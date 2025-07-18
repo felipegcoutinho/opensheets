1419556194a3e12c6b93084a52119e7fc15e0331
Felipe Coutinho <felipi@live.com>
2025-07-18 12:40:13 -0300

    decode city names

--- components/banner-data.tsx
+++ components/banner-data.tsx
@@ -1,10 +1,18 @@
 import { getUserName } from "@/app/actions/users/fetch_users";
 import { UseDates } from "@/hooks/use-dates";
+import { getLocationWeather } from "@/lib/weather";
+import { headers } from "next/headers";
 import Banner from "./banner-card";
 
 export default async function BannerData() {
   const { currentDate, friendlyDate } = UseDates();
   const userName = await getUserName();
+
+  const headersList = headers();
+  const locationWeather = await getLocationWeather(headersList);
+  const locationText = locationWeather
+    ? `${locationWeather.city}, ${locationWeather.temperature} °C`
+    : "Localização desconhecida";
 
   return (
     <Banner className="bg-contrast-foreground/10">
@@ -15,6 +23,7 @@ export default async function BannerData() {
             <span>{friendlyDate(currentDate)}.</span>
           </p>
         </div>
+        <p className="text-right text-sm text-muted-foreground">{locationText}</p>
       </div>
     </Banner>
   );
