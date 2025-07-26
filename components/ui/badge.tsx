906e7fff7cf858082a8abd5d940aedf9247f226f
Felipe Coutinho <felipi@live.com>
2025-07-26 12:58:13 -0300

    feat: adicionar componente Badge para exibir datas de vencimento
    Adiciona o componente Badge ao UsersCard para destacar as datas de
    vencimento dos cartões e boletos. O estilo do Badge foi ajustado para
    melhorar a visibilidade. Além disso, foram feitas pequenas alterações
    no estilo do componente Badge para incluir bordas.

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center justify-center rounded-md border-none px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+  "inline-flex items-center justify-center rounded-md  px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
   {
     variants: {
       variant: {
@@ -18,7 +18,7 @@ const badgeVariants = cva(
         destructive_lite:
           "border-destructive bg-destructive/5 text-destructive dark:text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+          "border text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
         receita:
           " bg-green-50  text-green-600 dark:text-green-100 dark:bg-green-600/20",
         despesa: " bg-red-50 text-red-600 dark:text-red-100 dark:bg-red-600/20",
