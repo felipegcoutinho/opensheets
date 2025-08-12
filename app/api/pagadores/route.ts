0d1dc269f6376fc001f10ea92b0f0916cd61be3b
Felipe Coutinho <felipi@live.com>
2025-08-12 17:42:01 +0000

    Implement code changes to enhance functionality and improve performance

--- app/api/pagadores/route.ts
+++
@@ -1,7 +0,0 @@
-import { getPagadores } from "@/app/actions/pagadores/fetch_pagadores";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const data = await getPagadores();
-  return NextResponse.json({ data });
-}
